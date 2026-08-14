9452c8274e437a19e05146bdfb2514f0f0968d8b
plasma19911 <308380239+plasma19911@users.noreply.github.com>
2026-08-14 08:53:21 +0200

    Aktualisiere PWA-Cache für neues Layout

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE='job-radar-v1';
-const SHELL=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon.svg'];
+const CACHE='job-radar-v2';
+const SHELL=['./','./index.html','./styles.css','./fixed.css','./app.js','./manifest.webmanifest','./icons/icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
