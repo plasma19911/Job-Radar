7f839d94a9b31c932628790cecc1e7700e2d1b69
plasma19911 <308380239+plasma19911@users.noreply.github.com>
2026-08-14 11:50:45 +0200

    Refresh cache for weekly new jobs popup

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE='job-radar-v3';
-const SHELL=['./','./index.html','./styles.css','./fixed.css','./app.js','./manifest.webmanifest','./icons/icon.svg'];
+const CACHE='job-radar-v4';
+const SHELL=['./','./index.html','./styles.css','./fixed.css','./new-jobs.css','./app.js','./new-jobs.js','./manifest.webmanifest','./icons/icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
